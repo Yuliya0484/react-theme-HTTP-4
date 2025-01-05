67bf08957e0737e4a43ed4945a7eec76f3cfac41
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-01-05 14:03:49 +0100

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import usersData from "./assets/users.json";
-import User from "./components/users/user.jsx";
+import { useEffect, useState } from "react";
+import { fetchArticles } from "./API/api";
+import Loader from "./components/Loader";
 
 const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    // axios
+    //   .get("https://hn.algolia.com/api/v1/search")
+    //   .then((res) => setArticles(res.data.hits));
+    const getArticlesData = async () => {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const { hits } = await fetchArticles("react");
+        setIsLoading(false);
+        setArticles(hits);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getArticlesData();
+  }, []);
   return (
-    <div className="wrapper">
-      {usersData.map((user) => (
-        <User key={user.id} {...user} />
-      ))}
+    <div>
+      <h2>Http Requests</h2>
+      <ul className="list">
+        {articles.map((item) => (
+          <li className="item" key={item.objectID}>
+            <a className="link" target="blank" href={item.url}>
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      {isLoading && <Loader />}
+      {isError && <h2>Something went wrong! Try again...</h2>}
     </div>
   );
 };
-
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./components/users/user.jsx";
 import "./index.css";
 import "modern-normalize";
 createRoot(document.getElementById("root")).render(
